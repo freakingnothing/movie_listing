03b23e04cd3d8b74b9297bfce4bbe48578c2447d
Elena <freaking.nothing@gmail.com>
2019-02-26 18:21:39 +0400

    Add simple methods to Lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,54 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_list, only: %i[show edit update destroy]
+
+  def index
+    @lists = current_user.lists.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = current_user.lists.new
+  end
+
+  def create
+    @list = current_user.lists.create(list_params)
+    
+    if @list.save
+      redirect_to @list
+    else
+      render 'new'  
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to @list
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @list.destroy
+      redirect_to 'index'
+    else
+      flash[:danger] = 'Error'
+    end    
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
