fa26b350d0667d5b858b53c773c31ff5a5fbd674
Elena <freaking.nothing@gmail.com>
2019-02-26 19:46:31 +0400

    Add simple methods to Movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,31 @@
 class MoviesController < ApplicationController
-end
+    before_action :authenticate_user!
+    before_action :find_list
+  
+    def create
+      @movie = Movie.create(movie_params)
+      if @movie.save
+        @list.movies << @movie
+        redirect_to_list_path(@list)
+      else
+        flash[:danger] = 'Error'
+      end
+    end
+  
+    private
+  
+    def movie_params
+      params.require(:movie).permit(:url,
+                                    :rus_title,
+                                    :original_title,
+                                    :release_year,
+                                    :rating,
+                                    :genre,
+                                    :url)
+    end
+  
+    def find_list
+      @list = List.find(params[:id])
+    end
+  end
+  
