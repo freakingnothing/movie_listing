66cb619c7a98297b7effff791bab57462e13e4f3
Elena <freaking.nothing@gmail.com>
2019-06-03 14:40:06 +0400

    Add methods to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,22 @@
 class Movie < ApplicationRecord
   has_many :listings
   has_many :lists, through: :listings
+
+  def find_existing_movie 
+    Movie.where(url: self.sanitize_url).first
+  end  
+
+  def is_new?
+    find_existing_movie.nil?
+  end
+
+  def sanitize_url
+    uri = URI.parse(self.url)
+    self.url = "http://#{uri.host + uri.path}"
+  end
+
+  def convert_imdb_url_to_mobile_imdb_url
+    uri = URI.parse(self.url)
+    self.mobile_url = "http://m.imdb.com#{uri.path}"
+  end
 end
