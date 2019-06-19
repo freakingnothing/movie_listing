97740effdbc32382087edbc9b5b893cebe0f5e2b
Elena <freaking.nothing@gmail.com>
2019-06-19 15:27:14 +0400

    Add country to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_103649) do
+ActiveRecord::Schema.define(version: 2019_06_19_112533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_103649) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "mobile_url"
+    t.string "country"
   end
 
   create_table "users", force: :cascade do |t|
